47727363df05f58bf933414e438719b050304956
tomerat <32419194+ReaperofSouls101@users.noreply.github.com>
2019-08-24 16:44:46 -0600

    Update move.py

--- app/move.py
+++ app/move.py
@@ -15,7 +15,6 @@ TAIL       = 4
 HEALTHLIM = 100
 game_state = ""
 directions = {'up': 0, 'down': 0, 'left': 0, 'right': 0}
-goodfood = []
 
 
 
@@ -28,17 +27,18 @@ def calculate_move(new_board, game_state):
 #    else:
 #    find_heads(game_state, new_board)
 
-    print(max(directions, key=lambda k: directions[k]))
-    print("UP", directions["up"])
-    print("DOWN", directions["down"])
-    print("LEFT", directions["left"])
-    print("RIGHT", directions["right"])
+#    print(max(directions, key=lambda k: directions[k]))
+#    print("UP", directions["up"])
+#    print("DOWN", directions["down"])
+#    print("LEFT", directions["left"])
+#    print("RIGHT", directions["right"])
     return max(directions, key=lambda k: directions[k])
 
 def find_food(game_state, board_matrix ):
     height = 11
     width = 11
-    print("Getting Food")
+    goodfood=""
+#    print("Getting Food")
     minsum = 1000
     y = game_state['you']["body"][0]["y"]
     x = game_state['you']["body"][0]["x"]
@@ -46,22 +46,22 @@ def find_food(game_state, board_matrix ):
     directions["down"] = 0
     directions["left"] = 0
     directions["right"] = 0
-    print("Your Coordinates "+ str(x) + ", " + str(y))
+#    print("Your Coordinates "+ str(x) + ", " + str(y))
     for food in game_state["board"]["food"]:
         tot = abs(food["x"] - x)
         tot += abs(food["y"] - y)
         if (tot < minsum):
             goodfood = food
             minsum = tot
-    print("Target Coordinates "+ str(goodfood["x"]) + ", " + str(goodfood["y"]))
+#    print("Target Coordinates "+ str(goodfood["x"]) + ", " + str(goodfood["y"]))
     best_move= find_path(game_state, board_matrix,x,y, goodfood["x"], goodfood["y"])
-    print("Best Move before dodge "+best_move)
+#    print("Best Move before dodge "+best_move)
     dodgeGrid = dodgeGridCreation(game_state, board_matrix, goodfood["x"], goodfood["y"], width, height)
     threeByThree = moveAura(x, y, dodgeGrid, width, height)
     remakeMove = ensureBestMove(threeByThree)
 
-    print(np.matrix(dodgeGrid))
-    print(np.matrix(threeByThree))
+#    print(np.matrix(dodgeGrid))
+#    print(np.matrix(threeByThree))
 
 
 #def find_heads(game_state, board_matrix ):
@@ -149,7 +149,7 @@ def find_path(game_state, board_matrix, x, y, targetx, targety):
     if (len(path) > 0):
         pathx = path[1][0]
         pathy = path[1][1]
-        print("Next Move Coordinates "+ str(path[1][0]) + ", " + str(path[1][1]))
+#        print("Next Move Coordinates "+ str(path[1][0]) + ", " + str(path[1][1]))
         y = game_state['you']["body"][0]["y"]
         x = game_state['you']["body"][0]["x"]
         # go up
