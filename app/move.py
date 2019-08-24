a4e590233c7878d0cb59cf2a68e4f4a4b9f07479
tomerat <32419194+ReaperofSouls101@users.noreply.github.com>
2019-08-24 12:38:27 -0600

    Update move.py

--- app/move.py
+++ app/move.py
@@ -61,7 +61,7 @@ def find_heads(game_state, board_matrix):
     for head in game_state["board"]["snakes"]:
         tot = abs(head["body"][0]['x'] - x)
         tot += abs(head["body"][0]['y'] - y)
-        if (tot < minsum):
+        if (tot < minsum) and (tot!=0):
             attackHead = head["body"][0]
             minsum = tot
     print("Coordinates "+ str(attackHead["x"]) + ", " + str(attackHead['y']))
