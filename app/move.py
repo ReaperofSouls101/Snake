d4d518a27de51209932c0690a9dcadaf5e1db67f
tomerat <32419194+ReaperofSouls101@users.noreply.github.com>
2019-08-24 15:11:08 -0600

    Update move.py

--- app/move.py
+++ app/move.py
@@ -101,7 +101,7 @@ def moveAura(yourX, yourY, occupiedSpaces):
 
     for i in range(len(aura)):
         for j in range(len(aura[i])):
-            aura[i][j] = occupiedSpaces[x+i-1][y+j+1]
+            aura[i][j] = occupiedSpaces[y+i+1][x+j-1]
 
     return aura
 
